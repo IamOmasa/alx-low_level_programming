46fa84178197570f31c470404e37b1fb30738777
Omary Ally <omaryally.em@gmail.com>
2023-12-14 13:12:39 -0800

    use puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-/*
- *C program to print output with puts
+/**
+ * main -entry point
  *
+ *Description: C program to print output with puts
+ *
+ *Return: Always 0 (Succes)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-		return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
