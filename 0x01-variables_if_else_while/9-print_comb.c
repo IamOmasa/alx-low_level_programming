0170d082a1e80ba87f49d766c0a2a0aaf02b005a
Omary Ally <omaryally.em@gmail.com>
2023-12-17 20:58:34 -0800

    put space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Assign random number to a variable n.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+
+		if (digit != 9)
+			putchar(',');
+			putchar(' ');
+		++digit;
+	}
+	putchar('\n');
+
+	return (0);
+}
