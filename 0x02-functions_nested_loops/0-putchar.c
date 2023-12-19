9f0311c8fe4aa2e19c3d0d999c8090b963ff7b3a
Omary Ally <omaryally.em@gmail.com>
2023-12-19 14:57:10 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Description: a program that prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (succcess)
+ */
+
+int main(void)
+{
+	char *pg = "_putchar";
+
+	printf("%s\n", pg);
+
+	return (0);
+}
