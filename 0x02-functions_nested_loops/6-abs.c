ae7ff308a229e2f6eeea9f046e033556c0b3e122
Omary Ally <omaryally.em@gmail.com>
2023-12-21 11:00:05 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_abs(int)- function to print absolute value
+ *
+ *@n: input integer
+ *
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
