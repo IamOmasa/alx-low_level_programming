29f200d51d0d0807321970de3a3209e28c86e02e
Omary Ally <omaryally.em@gmail.com>
2023-12-21 03:32:00 -0800

    check lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,12 @@
 /**
  *_islower - function to check lowercase characters
  *
- * parameter 'c' - this is an input value
+ * @c: parameter is an input value
  *
  * Return: Always 0 (success)
  */
 
-int _islower(int c) /* 'c' is an input value*/
+int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 		return (1);
