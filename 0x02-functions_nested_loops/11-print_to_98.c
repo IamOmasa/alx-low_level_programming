a4b9c0c96f1394b6dc225dbaa38da6cb6600dfb5
Omary Ally <omaryally.em@gmail.com>
2023-12-22 10:56:54 -0800

    up to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_to_98(int n)-function that prints all natural numbers
+ * from n to 98, followed by a new line
+ *@n: input paramenter
+ *
+ *return: valuesto 98
+ */
+
+void print_to_98(int n)
+{
+	int incr;
+
+	if (n > 98)
+		for (incr = n; incr > 98; --incr)
+			printf("%d, ", incr);
+	else
+		for (incr = n; incr < 98; ++incr)
+			printf("%d, ", count);
+	printf("98\n");
+}
