94b1fec37ede715db161b363b7e022574284a4f8
Omary Ally <omaryally.em@gmail.com>
2023-12-22 10:32:50 -0800

    add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *add(int a, int b)-function to add two numbers
+ *
+ *@a: first parameter 
+ *@b: second parameter
+ *
+ *return: sum
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
