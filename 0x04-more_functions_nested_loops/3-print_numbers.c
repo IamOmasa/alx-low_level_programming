b17e18ebd50a089bcc58f273d8979789f1d63d0c
Omary Ally <omaryally.em@gmail.com>
2023-12-24 10:55:44 -0800

    0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,13 +9,12 @@
 
 void print_numbers(void)
 {
-	int num =  0;
+	int num;
 
-	while (num>=0 && num <= 9)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar(num + 48);
-		num++;
 	}
 
-	_putchar("\n")
+	_putchar("\n");
 }
