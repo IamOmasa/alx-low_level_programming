19279d589062198b726f875b4bac1498465547ff
Omary Ally <omaryally.em@gmail.com>
2023-12-27 01:23:45 -0800

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,14 +10,14 @@
 
 void print_line(int n)
 {
-	int i, n;
+	int i;
 
-	for (i = 1; i <=  n; i++)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		if (n <= 0)
-			_putchar("\n");
-		else
-			_putchar(95);
+		for (i = 1; i <= n; ++i)
+			_putchar('_');
+		_putchar('\n');
 	}
-	_putchar("\n");
 }
