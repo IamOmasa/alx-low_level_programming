64bca4be97fa47d5f665c6b5e1430d8f9b2c3a8e
Omary Ally <omaryally.em@gmail.com>
2023-12-27 00:34:37 -0800

    minor changes

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,9 +6,9 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+int _putchar(char c);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-int _putchar(char c);
 
 #endif /* MAIN_H */
