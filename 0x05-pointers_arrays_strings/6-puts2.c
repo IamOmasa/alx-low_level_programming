6e02e8b12a6ef213a0ba0053ccced2269188a7ec
Omary Ally <omaryally.em@gmail.com>
2024-01-07 12:03:30 -0800

    put 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,7 @@
 #include"main.h"
 
 /**
- * puts2 - prints every other character of a string
- * 
+ * puts2 - prints every character of a string
  *
  * @str: string parameter input
  *
@@ -13,8 +12,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for(let i = 0; str[i] != '\0'; ++i){
-		if(i % 2 == 0)
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
