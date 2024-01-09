db784260ae09298bc7401a3292ca243ddcc6c79a
Omary Ally <omaryally.em@gmail.com>
2024-01-09 06:21:56 -0800

    search character

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *_strpbrk - function locates the first occurrence in the string s
+ * of any of the bytes in the string accept
+ *
+ *@s: string pointer to search
+ *
+ *@accept: matching string pointer
+ *
+ *Return: a pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept != '\0')
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+			j++;
+		}
+		i++;
+	}
+	return ('\0');
+}
