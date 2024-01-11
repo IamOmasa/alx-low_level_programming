cc678c1d86031e06e2458ef95225104c9b63ba66
Omary Ally <omaryally.em@gmail.com>
2024-01-11 11:25:13 -0800

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_pow_recursion - function return the value of x raised to the power of y
+ *
+ *@x: input
+ *@y: power
+ *
+ *Return: value of x power y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y % 2 == 0)
+	{
+		even = _pow_recursion(x, y / 2);
+		return (even * even);
+	}
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
