7800f9628185e29213c50862a7a9ac8e0fdb24ce
Omary Ally <omaryally.em@gmail.com>
2024-02-14 21:05:51 -0800

    print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program that print its name
+ * @argc: hold number of arguments passed
+ * @argv: array pointer that holds the argument passed
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	int i;
+
+	printf("%s\n", argv[i]);
+
+	return (0);
+}
