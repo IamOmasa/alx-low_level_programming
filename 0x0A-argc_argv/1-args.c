0a17901fff81c3ac357776414b4e3e8d5f37eb25
Omary Ally <omaryally.em@gmail.com>
2024-02-14 21:26:20 -0800

    number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program that print number of argument
+ * @argc: number of argument
+ * @argv: array pointer that holds the argument passed
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, __attribute__((unused)), char *argv[])
+{
+	ptintf("%s\n", argc);
+
+	return (0);
+
+}
