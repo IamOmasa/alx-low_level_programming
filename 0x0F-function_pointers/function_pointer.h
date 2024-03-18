b92d854063f34fe57c36e8ec1db4f38ef6016762
Omary Ally <omaryally.em@gmail.com>
2024-03-18 01:22:02 -0700

    my name is

--- 0x0F-function_pointers/function_pointer.h
+++ 0x0F-function_pointers/function_pointer.h
@@ -3,15 +3,4 @@
 
 void print_name(char *name, void (*f)(char *));
 
-/**
- * enum bool - typedef function for bool
- * @TRUE: 1
- * @FALSE: 0
-*/
-typedef enum bool
-{
-	TRUE = 1,
-	FALSE = 0
-} bool;
-
 #endif /* FUNCTION_POINTERS_H */
