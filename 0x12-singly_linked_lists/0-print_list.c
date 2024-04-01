54c451feedf037549c246554838fe346923c2e6c
Omary Ally <omaryally.em@gmail.com>
2024-04-01 12:27:32 -0700

    print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,23 +6,10 @@
  *@h: pointer to the head of next list
  * Return: node
  */
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * 
- */
-typedef struct listint_s
-{
-    int n;
-    struct listint_s *next;
-} listint_t;
 
 size_t print_list(const listint_t *h)
 {
-	
+
 	size_t count = 0;
 
 	while (h != NULL)
