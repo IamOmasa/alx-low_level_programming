55742e380c50af838907c69e70da96f6a26c86b2
Omary Ally <omaryally.em@gmail.com>
2024-05-24 01:59:02 -0700

    init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,7 @@
 /**
  *init_dog - initialize struct dog
  *
- *@d: input ponter d to sturct dog
+ *@d: input pointer d to sturct dog
  *@name: dog name input
  *@age: dog age input
  *@owner: dog owner input
@@ -11,6 +11,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
