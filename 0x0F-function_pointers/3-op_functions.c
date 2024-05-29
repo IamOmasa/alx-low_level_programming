c6d11f7af43f99b7b11d51cbeae6cb792becd2cb
Omary Ally <omaryally.em@gmail.com>
2024-05-28 21:49:20 -0700

    math operation

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+/**
+ *op_add - sum @a and @b
+ *
+ * @a: input a
+ * @b: input b
+ *
+ * Return: sum of @a and @b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - substract @a and @b
+ *
+ * @a: input a
+ * @b: input b
+ *
+ * Return: difference of @a and @b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - multiply @a and @b
+ *
+ * @a: input a
+ * @b: input b
+ *
+ * Return: product of @a and @b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - divide @a and @b
+ *
+ * @a: input a
+ * @b: input b
+ *
+ *Return: qutient of @a and @b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - gets the reminder of @a and @b
+ *
+ * @a: input a
+ * @b: input b
+ *
+ * Return: remainder
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
