59d2473f83640f303b7d4c0e27a9c70f66b7bc7a
Omary Ally <omaryally.em@gmail.com>
2024-05-30 21:43:49 -0700

    count nodes

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ *list_len - function that returns the number of elements
+ *
+ * @h: head
+ *
+ *Return: size of list
+ */
+
+size_t list_len(const list_t *h)
+{
+	int node_size = 1;
+
+	if (h == NULL)
+		return (0);
+	while(h->next != NULL)
+	{
+		/* iterate to next node */
+		h = h->next;
+
+		/* count */
+		++node_size;
+
+	}
+
+	return (node_size);
+}
