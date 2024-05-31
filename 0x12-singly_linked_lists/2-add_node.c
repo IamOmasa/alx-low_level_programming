acb94c2da26350294936a875f05f63f67400c176
Omary Ally <omaryally.em@gmail.com>
2024-05-31 00:26:35 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -25,8 +25,10 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node = malloc(sizeof(list_t));
 
-	if (new_node == NULL)
-		return (NULL);
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
 
 	new_node->str = strdup(str);
 	new_node->len = str_len;
