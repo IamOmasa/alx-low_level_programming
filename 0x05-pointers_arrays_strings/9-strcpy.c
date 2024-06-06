3a75637ce7f2adf5b14e888d40405e5b34fd552f
root <root@omasa>
2024-06-06 05:32:18 -0400

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strcpy -  a function that copies the string pointed to by src
+ *
+ * @dest: pointer to copied string
+ * @src: pointer to string
+ *
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	size_t i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
