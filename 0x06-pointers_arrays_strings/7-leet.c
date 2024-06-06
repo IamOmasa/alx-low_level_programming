dced8a6e56034c3204894133b7f4f4dbe4677fe2
root <root@omasa>
2024-06-06 08:25:54 -0400

    encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,30 +10,29 @@
 
 char *leet(char *c)
 {
-	int i, s = 0;
-	int sl[] = {97, 101, 111, 116, 108};
-	int ul[] = {65, 69, 79, 84, 76};
-	int n[] = {52, 51, 48, 55, 49};
+	int i, j;
+	char leet_ref[10] = "aAeEoOtTlL";
+	char leet_replace[10] = "4433007711";
 
 	/*iterate values in array c*/
-	while (c[s] != '\0')
+	/**
+	* loop through array value five times
+	* to check if the value is equal to
+	* any small letter in array sl or
+	* upper letter in array ul and if it
+	* is replace it with the value in array n
+	*/
+	for (i = 0; c[i] != '\0'; i++)
 	{
-i		/**
-		 * loop through array value five times
-		 * to check if the value is equal to
-		 * any small letter in array sl or
-		 * upper letter in array ul and if it
-		 * is replace it with the value in array n
-		*/
-		for (i = 0; i < 5; i++)
+		for (j = 0; j < 10; j++)
 		{
-			if (c[s] == sl[i] || c[s] == ul[i])
+			if (c[i] == leet_ref[j])
 			{
-				c[s] = n[i];
+				c[i] = leet_replace[j];
 				break;
 			}
 		}
-		s++;
+
 	}
 	return (c);
 }
